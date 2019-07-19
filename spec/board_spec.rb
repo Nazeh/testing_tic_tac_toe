662edef5ddba9bfc4f1c8b56d24468be756e3d09
Nazeh <ar.nazeh@gmail.com>
2019-07-19 19:29:39 +0300

    use random values to test all cases

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,64 +1,82 @@
 # frozen_string_literal: true
 
-require_relative '../lib/board.rb'
+require_relative '../lib/board'
 
 RSpec.describe Board do
-  let(:new_board) { Board.new }
+  let(:board) { subject.board }
+  let(:mark) { %w[X O].sample }
 
   describe '#initialize' do
-    subject { new_board.board }
-    it { should be_eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) }
+    it 'will initiate readable 2D array attribute @board' do
+      expect(subject.board).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    end
+  end
+
+  # helper methods
+  def row(cell)
+    (cell - 1) / 3
+  end
+
+  def col(cell)
+    (cell - 1) % 3
   end
 
   describe '#update' do
-    let(:mark) { 'X' }
-    subject { new_board.board }
-
-    context 'When mark cell 4, board:' do
-      let(:cell) { 4 }
-      before { new_board.update(cell, mark) }
-      it { should be_eql([[1, 2, 3], ['X', 5, 6], [7, 8, 9]]) }
-    end
-
-    context 'When mark cell 9, board:' do
-      let(:cell) { 9 }
-      before { new_board.update(cell, mark) }
-      it { should be_eql([[1, 2, 3], [4, 5, 6], [7, 8, 'X']]) }
-    end
-
-    context 'When mark cell 1, while cell 5 is marked board:' do
-      let(:cell) { 1 }
-      before { new_board.update(5, 'O') }
-      before { new_board.update(cell, mark) }
-      it { should be_eql([['X', 2, 3], [4, 'O', 6], [7, 8, 9]]) }
+    cell = (1..9).to_a.sample
+    it "will update any cell #{cell} in board" do
+      subject.update(cell, mark)
+      expect(board[row(cell)][col(cell)]).to eq(mark)
     end
   end
 
   describe '#get_row_col_diagonals' do
-    subject { new_board.get_row_col_diagonals(cell) }
-    before { new_board.update(1, 'O') }
-    before { new_board.update(5, 'X') }
-    before { new_board.update(3, 'O') }
-
-    context 'When cell is 5 (row 2 col 2)' do
-      let(:cell) { 5 }
-      it { should be_eql([[4, 'X', 6], [2, 'X', 8], ['O', 'X', 9], ['O', 'X', 7]]) }
+    context 'When cell == 5' do
+      cell = 5
+      it 'will return row, col, and two diagonals' do
+        expect(subject.get_row_col_diagonals(cell)).to eq(
+          [
+            [board[row(cell)][0], board[row(cell)][1], board[row(cell)][2]],
+            [board[0][col(cell)], board[1][col(cell)], board[2][col(cell)]],
+            [board[0][0], board[1][1], board[2][2]],
+            [board[0][2], board[1][1], board[2][0]]
+          ]
+        )
+      end
     end
-    context 'When cell is 1 (row 1 col 1)' do
-      let(:cell) { 1 }
-      it { should be_eql([['O', 2, 'O'], ['O', 4, 7], ['O', 'X', 9]]) }
+    context 'When cell is even' do
+      cell = [2, 4, 6, 8].sample
+      it 'will return row col only' do
+        expect(subject.get_row_col_diagonals(cell)).to eq(
+          [
+            [board[row(cell)][0], board[row(cell)][1], board[row(cell)][2]],
+            [board[0][col(cell)], board[1][col(cell)], board[2][col(cell)]]
+          ]
+        )
+      end
     end
-    context 'When cell is 7 (row 3 col 1)' do
-      let(:cell) { 7 }
-      it { should be_eql([[7, 8, 9], ['O', 4, 7], ['O', 'X', 7]]) }
+    context 'When cell is 1 or 9' do
+      cell = [1, 9].sample
+      it 'will return row col and left to right diagonal' do
+        expect(subject.get_row_col_diagonals(cell)).to eq(
+          [
+            [board[row(cell)][0], board[row(cell)][1], board[row(cell)][2]],
+            [board[0][col(cell)], board[1][col(cell)], board[2][col(cell)]],
+            [board[0][0], board[1][1], board[2][2]]
+          ]
+        )
+      end
     end
-    context 'When cell is 3 (row 1 col 3)' do
-      let(:cell) { 3 }
-      it { should be_eql([['O', 2, 'O'], ['O', 6, 9], ['O', 'X', 7]]) }
-    end
-    context 'When cell is even (row 2 col 3)' do
-      let(:cell) { 6 }
-      it { should be_eql([[4, 'X', 6], ['O', 6, 9]]) }
+    context 'When cell is 3 or 7' do
+      cell = [3, 7].sample
+      it 'will return row col and right to left diagonal' do
+        expect(subject.get_row_col_diagonals(cell)).to eq(
+          [
+            [board[row(cell)][0], board[row(cell)][1], board[row(cell)][2]],
+            [board[0][col(cell)], board[1][col(cell)], board[2][col(cell)]],
+            [board[0][2], board[1][1], board[2][0]]
+          ]
+        )
+      end
     end
   end
 end
